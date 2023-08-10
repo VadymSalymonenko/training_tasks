5d8f955d975336eece10be2bb287fb1792e212c8
Vadym Salymonenko <vadym95201@gmail.com>
2023-08-10 19:12:59 +0300

    added code and Cmake for task 4.

---
+++ 4/src/part2.c
@@ -0,0 +1,23 @@
+// Link: https://adventofcode.com/2022/day/4
+
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("../data.txt", "r");
+    if (file == NULL) {
+        printf("Failed to open file\n");
+        return -1;
+    }
+
+    int start1, end1, start2, end2;
+    int count = 0;
+    while (fscanf(file, "%d-%d,%d-%d\n", &start1, &end1, &start2, &end2) != EOF) {
+        if ((end1 >= start2 && start1 <= end2) || (end2 >= start1 && start2 <= end1)) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    fclose(file);
+    return 0;
+}
